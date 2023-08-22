e52ffcb746e10d0017d6cf5903b5221c57b79f0c
jhoncastro28 <jhonmancipe04@gmail.com>
2023-08-21 22:37:14 -0500

    Actividad Estructuras Dinamicas

--- src/Stack/model/Model.java
+++ src/Stack/model/Model.java
@@ -6,6 +6,46 @@ import java.util.*;
  * @author jhonm
  */
 public class Model {
+    
+    public double evaluateExpression(String expression) {
+        Stack<Double> operandStack = new Stack<>();
+        Stack<Character> operatorStack = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                StringBuilder numBuilder = new StringBuilder();
+                numBuilder.append(ch);
+
+                while (i + 1 < expression.length() && (Character.isDigit(expression.charAt(i + 1)) || expression.charAt(i + 1) == '.')) {
+                    numBuilder.append(expression.charAt(i + 1));
+                    i++;
+                }
+
+                double num = Double.parseDouble(numBuilder.toString());
+                operandStack.push(num);
+            } else if (ch == '(') {
+                operatorStack.push(ch);
+            } else if (ch == ')') {
+                while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+                    performOperation(operandStack, operatorStack);
+                }
+                operatorStack.pop(); // Expulsa el parentesis de la pila de operadores
+            } else if (isOperator(ch)) {
+                while (!operatorStack.isEmpty() && hasHigherPrecedence(operatorStack.peek(), ch)) {
+                    performOperation(operandStack, operatorStack);
+                }
+                operatorStack.push(ch);
+            }
+        }
+
+        while (!operatorStack.isEmpty()) {
+            performOperation(operandStack, operatorStack);
+        }
+
+        return operandStack.pop();
+    }
 
     public static boolean isOperator(char ch) {
         return ch == '+' || ch == '-' || ch == '*' || ch == '/';
--- src/Stack/presenter/ExpressionEvaluator.java
+++ src/Stack/presenter/ExpressionEvaluator.java
@@ -33,7 +33,7 @@ public class ExpressionEvaluator {
             switch (option) {
                 case 1:
                     expression = view.readData("Ingrese la expresion: ");
-                    result = evaluateExpression(expression);
+                    result = model.evaluateExpression(expression);
                     break;
                 case 2:
                     view.showMessage("Result: " + result);
@@ -53,46 +53,6 @@ public class ExpressionEvaluator {
                 + "-- MENU PRINCIPAL -- \n1. Evaluar una expresion matematica"
                 + "\n2. Mostrar el resultado \n3. Salir \nDigite Eleccion: ");
         return option;
-    }
-
-    public double evaluateExpression(String expression) {
-        Stack<Double> operandStack = new Stack<>();
-        Stack<Character> operatorStack = new Stack<>();
-
-        for (int i = 0; i < expression.length(); i++) {
-            char ch = expression.charAt(i);
-
-            if (Character.isDigit(ch)) {
-                StringBuilder numBuilder = new StringBuilder();
-                numBuilder.append(ch);
-
-                while (i + 1 < expression.length() && (Character.isDigit(expression.charAt(i + 1)) || expression.charAt(i + 1) == '.')) {
-                    numBuilder.append(expression.charAt(i + 1));
-                    i++;
-                }
-
-                double num = Double.parseDouble(numBuilder.toString());
-                operandStack.push(num);
-            } else if (ch == '(') {
-                operatorStack.push(ch);
-            } else if (ch == ')') {
-                while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
-                    model.performOperation(operandStack, operatorStack);
-                }
-                operatorStack.pop(); // Expulsa el parentesis de la pila de operadores
-            } else if (model.isOperator(ch)) {
-                while (!operatorStack.isEmpty() && model.hasHigherPrecedence(operatorStack.peek(), ch)) {
-                    model.performOperation(operandStack, operatorStack);
-                }
-                operatorStack.push(ch);
-            }
-        }
-
-        while (!operatorStack.isEmpty()) {
-            model.performOperation(operandStack, operatorStack);
-        }
-
-        return operandStack.pop();
     }
     
 }
